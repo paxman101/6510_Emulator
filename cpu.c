84424d5fa1cdad1692b0bf6e43f5482e7b84a4ce
Paxton Schweigert <paxmanrocks@gmail.com>
2022-03-01 12:36:33 -0800

    add status flags

--- cpu.c
+++ cpu.c
@@ -5,10 +5,26 @@
 #include "cpu.h"
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* 6510 Registers (https://www.c64-wiki.com/wiki/CPU_6510-Register_set) */
 static u_int16_t PC;
 static u_int8_t  STATUS;
-static u_int8_t  ACCUMULATOR;
+static u_int8_t  A;
 static u_int8_t  X;
 static u_int8_t  Y;
+
+/* Status Flags */
+enum STATUS_FLAG {
+    STAT_CARRY = 0,
+    STAT_ZERO  = 1,
+    STAT_IRQ_DISABLE = 2,
+    STAT_DEC_MODE = 3,
+    STAT_BRK_COMMAND = 4,
+    STAT_OVERFLOW = 6,
+    STAT_NEGATIVE = 7
+};
+
+static inline bool get_status_flag(enum STATUS_FLAG flag) {
+    return (STATUS ^ (1 << flag)) >> flag;
+}
