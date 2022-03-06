a031d301918d46b846362d438f7d3da8ce6389a9
Paxton Schweigert <paxmanrocks@gmail.com>
2022-03-05 22:09:47 -0800

    add test file nestest.nes and start adding some tests in main.c

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+#include "cpu.h"
+#include "memory.h"
+
 int main() {
-    printf("Hello, World!\n");
+    initMemory((1 << 16) - 1);
+
+    loadBinFile("nestest.nes", 0x8000, 0x0010, 0x4000);
+    loadBinFile("nestest.nes", 0xC000, 0x0010, 0x4000);
+
+    u_int8_t *mem = getMemoryPtr(INT_RESET+1);
+    *mem = 0xC0;
+    *(mem - 1) = 0x00;
+
+    initCPU();
+    startCPUExecution();
+
     return 0;
 }
